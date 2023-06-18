b985b9ee1d27c01e822820e72b7ad59d947ff8d3
Dodoslav Novak <dodoslavn@gmail.com>
2023-06-18 12:16:00 +0200

    fix

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -6,12 +6,12 @@ cd $(dirname $0)
 . ../conf/settings.sh
 
 if ! [ -a "../conf/apache_servers.csv" ]
-	then
-	echo ERROR: File with server list not found!
-	exit 1
-	fi
+        then
+        echo ERROR: File with server list not found!
+        exit 1
+        fi
 
 for SERVER in $(grep -v ^# "../conf/apache_servers.csv" | cut -d';' -f1)
-	do
-	$(pwd)/main.sh $SERVER > ../logs/$SERVER".log"
-	done
+        do
+        sudo /bin/su - root -c "$(pwd)/main.sh $SERVER" > ../logs/$SERVER'.log'
+        done
