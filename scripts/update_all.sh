28cd395710500a97b87f4ada75c088f6ee55f636
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-16 11:53:01 +0200

    Update update_all.sh

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -9,25 +9,25 @@ if [ -z "$CONF_DIR" ]
 	CONF_DIR=$(pwd)"/conf/"
 	fi
 
-if ! [ -a $CONF_DIR"settings.sh" ]
+if ! [ -a "$CONF_DIR""settings.sh" ]
 	then
 	echo ERROR: Settings file not found!
 	echo $CONF_DIR"settings.sh"
 	exit 1
 	fi
 
-. $CONF_DIR"settings.sh"
+. "$CONF_DIR""settings.sh"
 
-if ! [ -a $CONF_DIR"apache_servers.csv" ]
+if ! [ -a "$CONF_DIR""apache_servers.csv" ]
         then
         echo ERROR: File with server list not found!
         exit 1
         fi
 
-mkdir -p $CONF_DIR'../logs/'
-rm -f $CONF_DIR'../logs/'$SERVER'.log'
+mkdir -p "$CONF_DIR"'../logs/'
+rm -f "$CONF_DIR"'../logs/'$SERVER'.log'
 
-for SERVER in $(grep -v ^# $CONF_DIR"apache_servers.csv" | cut -d';' -f1)
+for SERVER in $(grep -v ^# "$CONF_DIR""apache_servers.csv" | cut -d';' -f1)
         do
-        sudo /bin/su - root -c "$(pwd)/scripts/main.sh $SERVER" | tee -a $CONF_DIR'../logs/'$SERVER'.log'
+        sudo /bin/su - root -c "$(pwd)/scripts/main.sh $SERVER" | tee -a "$CONF_DIR"'../logs/'$SERVER'.log'
         done
