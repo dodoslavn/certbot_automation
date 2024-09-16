4db72aeeaff47e03df345e4c56f956f754efe27b
Dodoslav Novak <34769717+dodoslavn@users.noreply.github.com>
2024-09-16 12:07:31 +0200

    Update update_all.sh

--- scripts/update_all.sh
+++ scripts/update_all.sh
@@ -2,24 +2,24 @@
 
 #cd '$(dirname $0)'
 
-if ! [ -a '../conf/settings.sh' ]
+if ! [ -a 'conf/settings.sh' ]
 	then
 	echo "ERROR: Settings file not found!"
 	exit 1
 	fi
 
-if ! [ -a '../conf/apache_servers.csv' ]
+if ! [ -a 'conf/apache_servers.csv' ]
         then
         echo "ERROR: File with server list not found!"
         exit 2
         fi
 
-.  "../conf/settings.sh"
+.  "conf/settings.sh"
 FILEN="_"$( date '+%Y.%m.%d_%H:%M:%S' )".log"
 
 mkdir -p "../logs/"
 
-for SERVER in $(grep -v ^# "../conf/apache_servers.csv" | cut -d';' -f1)
+for SERVER in $(grep -v ^# "conf/apache_servers.csv" | cut -d';' -f1)
         do
-        sudo /bin/su - root -c "$(pwd)/main.sh $SERVER" | tee -a $SEVER"_"$FILEN
+        sudo /bin/su - root -c "$(pwd)/scripts/main.sh $SERVER" | tee -a $SEVER"_"$FILEN
         done
